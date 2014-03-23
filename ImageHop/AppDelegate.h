862b58a7b73cb146137fb9dc68746bded9644061
James Newman <jamesmnewman1@gmail.com>
2014-03-22 19:54:59 -0700

    local git commit

---
+++ ImageHop/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ImageHop
+//
+//  Created by James Newman on 3/17/14.
+//  Copyright (c) 2014 James Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ImageHop/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageHop
+//
+//  Created by James Newman on 3/17/14.
+//  Copyright (c) 2014 James Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
